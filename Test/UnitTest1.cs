3fb547bbc05745325e4fa2ae00d501eacf5d4f4c
TorLindstrom <tor.lindstrom@winlas.se>
2024-11-06 14:29:24 +0100

    refactor: remove test that checks return type

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -4,14 +4,6 @@ namespace Test;
 
 public class UnitTest1
 {
-    [Fact(DisplayName = "Metoden returnerar en sträng")]
-    public void HighAndLow_ReturnsResultAsString()
-    {
-        var result = Kata.HighAndLow("1 2 3 4 5");
-
-        Assert.IsType<string>(result);
-    }
-
     [Fact(DisplayName = "Om input är null returneras specialvärdet 0")]
     public void HighAndLow_InputIsNull_ReturnsZero()
     {
