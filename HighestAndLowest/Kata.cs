8258e8e0bde1b09b33369a3bc1321e9e58c2a53d
TorLindstrom <tor.lindstrom@winlas.se>
2024-11-06 14:38:46 +0100

    refactor: validation logic to own methodd

--- HighestAndLowest/Kata.cs
+++ HighestAndLowest/Kata.cs
@@ -5,6 +5,11 @@ public class Kata
 {
 	public static string HighAndLow(string input)
 	{
-		return input == null || string.IsNullOrWhiteSpace(input) ? "0" : "1,1";
+		return IsInputInvalid(input) ? "0" : "1,1";
+	}
+
+	private static bool IsInputInvalid(string input)
+	{
+		return input == null || string.IsNullOrWhiteSpace(input);
 	}
 }
